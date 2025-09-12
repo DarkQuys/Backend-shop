b88053194cd0b07bd5da1d2624ac5393f2ef47b1
DarkQuys <quockp2@gmail.com>
2025-09-12 23:22:21 +0700

    up

--- index.js
+++ index.js
@@ -7,19 +7,17 @@ const router = require('./routes')
 
 
 const app = express()
-// app.use(cors({
-//     origin : process.env.FRONTEND_URL,
-//     credentials : true
-// }))
 app.use(cors({
-    origin: "*"
-}));
+    origin : "http://localhost:3000/" ,
+    credentials : true
+}))
+
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api",router)
 
-const PORT = 8080 || process.env.PORT
+const PORT = process.env.PORT || 8080
 
 
 connectDB().then(()=>{
